791b1d588ccfdab31614263a6468bd311dce0e2c
eg-lee <edward.lee@vunlab.com>
2014-03-25 09:41:43 -0500

    nav modifications

--- gumby-claymate/js/index.js
+++ gumby-claymate/js/index.js
@@ -12,17 +12,39 @@ $( document ).ready(function() {
   
     $($navIcon).on('click', function(e) {  
         e.preventDefault(); 
-        $nav.toggleClass('nav-add-margin');
-        $navContainer.toggleClass('nav-remove-padding');
-        $nav.slideToggle();  
+        if ($nav.is(':hidden')) {
+          $nav.toggleClass('nav-add-margin');
+          $navContainer.toggleClass('nav-remove-padding');
+          $nav.slideToggle();  
+        } else {
+          $nav.slideToggle(); 
+          $nav.toggleClass('nav-add-margin');
+          $navContainer.toggleClass('nav-remove-padding');
+        }
+ 
     });  
 
     $(window).resize(function(){  
       var width = $(window).width();  
     
-      if(width > 767 && $nav.is(':hidden')) {  
-        $nav.removeAttr('style');  
-      }  
+    //&& $nav.is(':hidden')
+
+      if (width > 767) {
+      	if ($nav.is(':hidden')) {
+      	  $nav.removeAttr('style');
+          if ($nav.hasClass('nav-add-margin') && 
+        	  $nav.hasClass('nav-remove-padding')) {
+            $nav.removeClass('nav-add-margin');
+      	    $nav.removeClass('nav-remove-padding');
+      	  }
+        } else {
+           $nav.removeClass('nav-add-margin');
+        }
+      } else if (width < 767) {
+      	if (!$nav.is(':hidden') && !$nav.hasClass('nav-add-margin')) {
+      	  $nav.addClass('nav-add-margin');
+      	}
+      }
 	}); 
 
 
